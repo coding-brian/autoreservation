b9c9ca574731407b281465af91d49d8148cfbb96
Andy <ay83103@gmail.com>
2021-08-26 00:51:16 +0800

    初始化

---
+++ Model/Line/LineMessage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Line
+{
+    public class LineMessage
+    {
+        public String destination { get; set; }
+
+        public List<Event> events { get; set; }
+
+    }
+
+    public class Event 
+    { 
+        public String replyToken { get; set; }
+
+        public String type { get; set; }
+
+        public String mode { get; set; }
+
+        public int timestamp { get; set; }
+
+        public Source source { get; set; }
+
+    }
+
+    public class Source
+    {
+        public String type { get; set; }
+
+        public String userId { get; set; }
+    }
+}
