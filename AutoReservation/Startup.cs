1a2b4d40ea1ab36cc993e646dd9fead59a037772
Andy <ay83103@gmail.com>
2021-09-07 23:33:18 +0800

    DI注入

--- AutoReservation/Startup.cs
+++ AutoReservation/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Service.WebAPIRequest;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +29,7 @@ namespace AutoReservation
             services.AddControllers();
             services.AddSwaggerGen();
             services.AddHttpClient();
+            services.AddScoped<IWebAPIRequest, WebAPIRequest>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
